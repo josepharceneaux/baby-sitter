91fa73b26467bc2a7d6fdbc3742cbc21b57d26be
Joseph Arceneaux <joe.arceneaux@gmail.com>
2019-05-16 14:47:51 -0500

    Add tests.

--- sitter.py
+++ sitter.py
@@ -3,6 +3,23 @@ Class for a baby sitter
 """
 
 
+
+def convert_to_european(time):
+    """
+    Convert a fixed number of hours into European time format
+    """
+    return time % 24
+
+
+def convert_from_european(time, next_day=False):
+    """
+    Given a European time format (0-23) convert into the total number of hours
+    """
+    if next_day:
+        return time + 24
+
+    return time
+
 class Sitter:
     """
     The payment due a sitter based upon the hours worked and the family they worked for
@@ -16,3 +33,8 @@ class Sitter:
         self.start_time = start_time
         self.hours_worked = hours_worked
         self.family = family
+
+    def calculate_payment(self):
+        """
+        Calculate the total amount due to this sitter based upon a family's hourly rates
+        """
--- tests/test_sitter.py
+++ tests/test_sitter.py
@@ -5,7 +5,7 @@ Tests for baby sitter kata
 
 import unittest
 
-from ..sitter import Sitter
+from ..sitter import Sitter, convert_to_european, convert_from_european
 from ..family import Family, MINIMUM_START, MAXIMUM_END, FAMILY_B_RATES
 
 
@@ -23,29 +23,37 @@ class TestBabySitter(unittest.TestCase):
         self.sitter_start_time = 17
         self.hours_worked = 3
         self.family = Family(FAMILY_B_RATES)
-        self.sitter = Sitter(sitter_start_time, self.hours_worked, self.family)
+        self.sitter = Sitter(self.sitter_start_time, self.hours_worked, self.family)
 
     def test_family_creation(self):
         """
         Test correct creation of Family object
         """
+        self.assertTrue(self.maximum_work_hours == 11)
+        self.assertTrue(len(self.family.rate_dict) == 11)
 
     def test_sitter_creation(self):
         """
         Test correct creation of sitter object
         """
+        self.assertTrue(self.sitter.start_time == self.sitter_start_time)
 
     def test_convert_to_european(self):
         """
         Test converstion from absolute hours to European time format
         """
+        absolute = 27
+        self.assertTrue(convert_to_european(absolute) == 3)
 
     def test_convert_from_european(self):
         """
         Test converstion from absolute hours to European time format
         """
+        absolute = 17
+        self.assertTrue(convert_from_european(absolute) == 17)
 
     def test_sitter_payment(self):
         """
         Test correct calculation of sitter payment
         """
+        self.assertTrue(self.sitter.calculate_payment() == 3 * 15)
